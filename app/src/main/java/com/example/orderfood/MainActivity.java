d23e50b6bbdce1cccff7cd436937389d33949bf7
NGOCGIAU\dangv <dangvykhoi@gmail.com>
2024-10-28 15:55:11 +0700

    add firebase

--- app/src/main/java/com/example/orderfood/MainActivity.java
+++ app/src/main/java/com/example/orderfood/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.orderfood;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db.collection("user")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            // Hiển thị dữ liệu của từng document trong log
+                            Log.d("Firestore", document.getId() + " => " + document.getData());
+                        }
+                    } else {
+                        Log.w("Firestore", "Lỗi khi lấy dữ liệu", task.getException());
+                    }
+                });
     }
 }
