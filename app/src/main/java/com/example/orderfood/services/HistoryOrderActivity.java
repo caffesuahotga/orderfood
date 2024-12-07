3ce0d0823e65cd79271e938a1a037c1638b37917
NGOCGIAU\dangv <dangvykhoi@gmail.com>
2024-12-08 01:58:26 +0700

    update bar bottom

--- app/src/main/java/com/example/orderfood/services/HistoryOrderActivity.java
+++ app/src/main/java/com/example/orderfood/services/HistoryOrderActivity.java
@@ -3,6 +3,7 @@ package com.example.orderfood.services;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 
@@ -15,9 +16,12 @@ import com.example.orderfood.models.Order;
 import com.example.orderfood.models.dto.CartDTO;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HistoryOrderActivity  extends BaseTopBottomViewActivity  {
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +29,36 @@ public class HistoryOrderActivity  extends BaseTopBottomViewActivity  {
         getSupportActionBar().hide();
         getLayoutInflater().inflate(R.layout.activity_history_order, findViewById(R.id.content_frame_top_bot));
 
+
         BindData();
+
+        // Xử lý refresh
+        swipeRefreshLayout = findViewById(R.id.history_order_page_refresh);
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            // Show loading spinner
+            swipeRefreshLayout.setRefreshing(true);
+
+            // Call lại API hoặc logic tải dữ liệu trong background thread
+            new Thread(() -> {
+
+                loadHistoryOrder();
+
+                // Sau khi dữ liệu được tải, thực hiện thao tác trên UI thread
+                runOnUiThread(() -> {
+                    // Kết thúc hiệu ứng refresh
+                    swipeRefreshLayout.setRefreshing(false);
+                });
+            }).start();
+        });
     }
 
     private void BindData()
     {
         // lấy danh sách order của account hiện tại
         ArrayList<Order> odList = OrderUtil.getAllOrdersByAccountId(this);
+        ArrayList<Order> sortedList = (ArrayList<Order>) odList.stream()
+                .sorted(Comparator.comparingInt(Order::getId).reversed())
+                .collect(Collectors.toList());
 
         RecyclerView history_order = findViewById(R.id.history_order_container);
         history_order.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
@@ -40,7 +67,14 @@ public class HistoryOrderActivity  extends BaseTopBottomViewActivity  {
             odList = new ArrayList<>(); // Nếu danh sách trả về null, khởi tạo một ArrayList rỗng
         }
 
-        HistoryOrderAdapter historyOrderAdapter = new HistoryOrderAdapter(this, odList);
+        HistoryOrderAdapter historyOrderAdapter = new HistoryOrderAdapter(this, sortedList);
         history_order.setAdapter(historyOrderAdapter);
+    }
+
+    // cập nhật data khi kéo
+    private void loadHistoryOrder() {
+        runOnUiThread(() -> {
+            BindData();
+        });
     }
 }
