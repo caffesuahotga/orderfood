0a560c150a33786bb6af5577bd554f50fd9b21d7
NGOCGIAU\dangv <dangvykhoi@gmail.com>
2024-12-09 16:36:31 +0700

    add app shipper

---
+++ app/src/main/java/com/example/orderfood/services/ShipperHistoryOrderActivity.java
@@ -0,0 +1,14 @@
+package com.example.orderfood.services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShipperHistoryOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shipper_history_order);
+    }
+}
